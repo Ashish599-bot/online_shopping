796e94b8ae43c084e9006da05f8d3c2bfee16d1a
Ashish <ashishsky619gmail.com>
2025-11-19 11:53:59 +0600

    adding knex and migrate

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE,
+      host: "localhost",
+      port: 5433,
+    },
+  },
+};
